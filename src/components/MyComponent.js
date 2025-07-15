3c2cb4c44afb35ee687e6c823c26ad3d02240fd7
minh041104 <mlodt3@gmail.com>
2025-07-15 19:47:26 +0700

    test displayinfo

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,12 +1,15 @@
 // function component   
 import React from "react";
 import UserInfo from "./UserInfo";
+import DisplayInfo from "./DisplayInfo";
 // class component
 class MyComponent extends React.Component {
     render() {
         return (
         <div>
             <UserInfo/>
+            <br></br>
+            <DisplayInfo name={"minh"} age={21}/>
         </div>
         )
     }
