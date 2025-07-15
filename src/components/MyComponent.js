2bad3b14e19e07b6e267098ae2c10e3d313e770d
minh041104 <mlodt3@gmail.com>
2025-07-15 17:32:57 +0700

    update mycomponent

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,16 +2,20 @@
 import React from "react";
 // class component
 class MyComponent extends React.Component {
-  render() {
-    return (
-      <div>
-        hello
-        {
-            Math.random()
-        }
-      </div>
-    )
-  }
+
+    state = {
+        name: "minh",
+        address: "hanoi",
+        age: 20
+    }
+
+    render() {
+        return (
+        <div>
+            hello my name is {this.state.name} and i am {this.state.age} years old
+        </div>
+        )
+    }
 }
 
 export default MyComponent;
